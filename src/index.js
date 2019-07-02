dbc488c71265c013bafcad5f76373c0d23d7244a
Italo Abreu <italodw@gmail.com>
2019-07-02 00:30:12 -0300

    Modulo CORS adicionado

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const port = 9091;
 const app = express();
 
-app.listen(port, () => console.log(`
+app.use(cors());
+
+app.listen(port, () => process.stdout.write(`
   Server rodando.
   http://localhost:${port}
 `));
