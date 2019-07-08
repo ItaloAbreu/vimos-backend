1910848bba0be30de029a95a144deebfc0b9e7ab
Italo Abreu <italodw@gmail.com>
2019-07-08 19:41:54 -0300

    Uploads apenas de videos.

--- src/config/upload.js
+++ src/config/upload.js
@@ -1,6 +1,8 @@
 const multer = require('multer');
 const path = require('path');
 
+const acceptedExtensions = ['.mp4', '.avi', '.ogg'];
+
 module.exports = {
   storage: multer.diskStorage({
     destination: path.resolve(__dirname, '..', '..', 'uploads'),
@@ -8,4 +10,10 @@ module.exports = {
       callback(null, `${Date.now()}-${file.originalname.split(' ').join('-')}`);
     },
   }),
+  fileFilter(req, file, callback) {
+    if (acceptedExtensions.includes(path.extname(file.originalname))) {
+      return callback(null, true);
+    }
+    return callback(new Error('Wrong file type.'));
+  },
 };
--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,12 @@ const VideoController = require('./controllers/VideoController');
 const StreamVideos = require('./controllers/StreamVideos');
 
 const routes = new express.Router();
-const UploadVideo = multer(uploadsConfig);
+const UploadVideo = multer(uploadsConfig).single('video');
 
 routes.use('/', express.static(`${__dirname}/public`));
 
 routes.get('/list', VideoController.index);
-routes.post('/video/storage', UploadVideo.single('video'), VideoController.storage);
+routes.post('/video/storage', UploadVideo, VideoController.storage);
 routes.get('/video/:id', VideoController.show);
 routes.put('/video/:id', VideoController.update);
 routes.delete('/video/:id', VideoController.delete);
